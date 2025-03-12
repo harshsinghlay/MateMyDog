5968a9afa5584d05f6cbc9a6db0ea18d40e82a9b
harshsinghlay <harsh.singhlay@synlabs.io>
2025-03-12 11:33:44 +0530

    First Commit After reconnecting with existing repo

--- src/components/social/SocialFeed.tsx
+++ src/components/social/SocialFeed.tsx
@@ -31,9 +31,7 @@ export function SocialFeed() {
         setShowAuthModal(true);
         return;
       }
-      console.log("handleLike of SocialFeed");
       if (likeInProgress[postId]) return;
-
       setLikeInProgress((prev) => ({ ...prev, [postId]: true }));
       await likePost(postId);
       setLikeInProgress((prev) => ({ ...prev, [postId]: false }));
